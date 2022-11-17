5eef9cc52ac94bc22fd7a4073a7cb4e0fc156f55
Liiyuufff <118420422+Liiyuufff@users.noreply.github.com>
2022-11-17 15:47:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oujv56Z8b239dSg3Lf67ZsT4ZNTI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'wxid_4f594dc1750n22',
+      useTemplateId: 'yKaJgKJ6BksUpVITmgdo2L-K4TzcwejhXAPLX4QTB-E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-05',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'yKaJgKJ6BksUpVITmgdo2L-K4TzcwejhXAPLX4QTB-E',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '刘sir',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'wxid_4f594dc1750n22',
+      id: 'oujv56Z8b239dSg3Lf67ZsT4ZNTI',
     }
   ],
 
